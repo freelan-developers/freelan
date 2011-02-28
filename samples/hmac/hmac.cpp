b0f2418c97c7229f40c3c6cd9434c18a85e81100
Julien Kauffmann <julien.kauffmann@freelan.org>
2011-02-28 17:57:48 +0100

    Better code

--- samples/hmac/hmac.cpp
+++ samples/hmac/hmac.cpp
@@ -2,26 +2,53 @@
 
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <iomanip>
 #include <vector>
 
-int main()
+template <typename T>
+std::string to_hex(const T& begin, const T& end)
+{
+	std::ostringstream oss;
+
+	for (T i = begin; i != end; ++i)
+	{
+		oss << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(*i);
+	}
+
+	return oss.str();
+}
+
+std::string to_hex(const void* buf, size_t buf_len)
+{
+	return to_hex(static_cast<const unsigned char*>(buf), static_cast<const unsigned char*>(buf) + buf_len);
+}
+
+void hmac(const std::string& name, const std::string& key, const std::string& data, const EVP_MD* md)
 {
 	cryptopen::hash::hmac_context ctx;
 
+	ctx.initialize(key.c_str(), key.size(), md);
+	ctx.update(data.c_str(), data.size());
+	std::vector<unsigned char> hmac = ctx.finalize<unsigned char>();
+
+	std::cout << name << ": " << to_hex(hmac.begin(), hmac.end()) << std::endl;
+}
+
+int main()
+{
+	std::cout << "HMAC sample" << std::endl;
+	std::cout << "===========" << std::endl;
+	std::cout << std::endl;
+
 	const std::string key = "my secret key";
 	const std::string data = "some data";
-	std::vector<unsigned char> hmac(EVP_MD_size(EVP_sha256()));
 
-	ctx.initialize(key.c_str(), key.size(), EVP_sha256());
-	ctx.update(data.c_str(), data.size());
-	size_t cnt = ctx.finalize(&hmac[0], hmac.size());
+	std::cout << "Key: " << key << std::endl;
+	std::cout << "Data: " << data << std::endl;
+	std::cout << std::endl;
 
-	for (size_t i = 0; i < cnt; ++i)
-	{
-		printf("%02x ", hmac[i]);
-	}
-
-	printf("\n");
+	hmac("SHA-256", key, data, EVP_sha256());
 
 	return EXIT_SUCCESS;
 }
