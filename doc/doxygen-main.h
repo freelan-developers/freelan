36e7b9741f2e218a03f9c217a054a46931d35901
Julien Kauffmann <julien.kauffmann@freelan.org>
2011-03-05 11:47:35 +0100

    Better documentation generation

---
+++ doc/doxygen-main.h
@@ -0,0 +1,30 @@
+/**
+ * \file doxygen-main.h
+ * \brief Documentation main page.
+ * \author Julien Kauffmann <julien.kauffmann@freelan.org>
+ * \date 2011
+ */
+
+/**
+ * \mainpage libcryptopen Documentation
+ *
+ * \section section-intro Introduction
+ *
+ * This is the API of libcryptopen, an OpenSource multiplatform C++ wrapper around the OpenSSL crypto library.
+ * 
+ * \section section-build Build instructions
+ * 
+ * To build and install libcryptopen, see INSTALL file located in the source tree.
+ *  
+ * \section section-links Links
+ * 
+ * <ul>
+ * <li><a href="http://www.freelan.org">libcryptopen website</a></li>
+ * <li><a href="https://github.com/ereOn/libcryptopen">libcryptopen project page</a></li>
+ * </ul>
+ * 
+ * \section section-license License
+ * 
+ * libcryptopen is licensed under the <a href="http://www.gnu.org/licenses/gpl-3.0.html">GPLv3.0 license</a>.
+ */
+
