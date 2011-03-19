6d388136cc19e9a76884a7975a530c2d33396b5e
Julien Kauffmann <julien.kauffmann@freelan.org>
2011-03-19 16:09:24 +0100

    Added 509::certificate class base code

---
+++ include/cryptoplus/x509/certificate.hpp
@@ -0,0 +1,124 @@
+/*
+ * libcryptoplus - C++ portable OpenSSL cryptographic wrapper library.
+ * Copyright (C) 2010-2011 Julien Kauffmann <julien.kauffmann@freelan.org>
+ *
+ * This file is part of libcryptoplus.
+ *
+ * libcryptoplus is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * libcryptoplus is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * In addition, as a special exception, the copyright holders give
+ * permission to link the code of portions of this program with the
+ * OpenSSL library under certain conditions as described in each
+ * individual source file, and distribute linked combinations
+ * including the two.
+ * You must obey the GNU General Public License in all respects
+ * for all of the code used other than OpenSSL.  If you modify
+ * file(s) with this exception, you may extend this exception to your
+ * version of the file(s), but you are not obligated to do so.  If you
+ * do not wish to do so, delete this exception statement from your
+ * version.  If you delete this exception statement from all source
+ * files in the program, then also delete it here.
+ *
+ * If you intend to use libcryptoplus in a commercial software, please
+ * contact me : we may arrange this for a small fee or no fee at all,
+ * depending on the nature of your project.
+ */
+
+/**
+ * \file certificate.hpp
+ * \author Julien KAUFFMANN <julien.kauffmann@freelan.org>
+ * \brief A X509 certificate class.
+ */
+
+#ifndef CRYPTOPEN_X509_CERTIFICATE_HPP
+#define CRYPTOPEN_X509_CERTIFICATE_HPP
+
+#include "../error/cryptographic_exception.hpp"
+
+#include <openssl/x509.h>
+
+#include <boost/shared_ptr.hpp>
+
+namespace cryptoplus
+{
+	namespace x509
+	{
+		/**
+		 * \brief A X509 certificate.
+		 *
+		 * The certificate class represents a X509 certificate.
+		 *
+		 * A certificate instance has the same semantic as a X509* pointer, thus two copies of the same instance share the same underlying pointer.
+		 */
+		class certificate
+		{
+			public:
+
+				/**
+				 * \brief Get the raw X509 pointer.
+				 * \return The raw X509 pointer.
+				 * \warning The instance has ownership of the return pointer. Calling X509_free() on the returned value will result in undefined behavior.
+				 */
+				const X509* raw() const;
+
+				/**
+				 * \brief Get the raw X509 pointer.
+				 * \return The raw X509 pointer.
+				 * \warning The instance has ownership of the return pointer. Calling X509_free() on the returned value will result in undefined behavior.
+				 */
+				X509* raw();
+
+			private:
+
+				boost::shared_ptr<X509> m_x509;
+		};
+
+		/**
+		 * \brief Compare two certificate instances.
+		 * \param lhs The left argument.
+		 * \param rhs The right argument.
+		 * \return true if the two certificate instances share the same underlying pointer.
+		 */
+		bool operator==(const certificate& lhs, const certificate& rhs);
+
+		/**
+		 * \brief Compare two certificate instances.
+		 * \param lhs The left argument.
+		 * \param rhs The right argument.
+		 * \return true if the two certificate instances do not share the same underlying pointer.
+		 */
+		bool operator!=(const certificate& lhs, const certificate& rhs);
+
+		inline const X509* certificate::raw() const
+		{
+			return m_x509.get();
+		}
+		inline X509* certificate::raw()
+		{
+			return m_x509.get();
+		}
+		inline bool operator==(const certificate& lhs, const certificate& rhs)
+		{
+			return lhs.raw() == rhs.raw();
+		}
+		inline bool operator!=(const certificate& lhs, const certificate& rhs)
+		{
+			return lhs.raw() != rhs.raw();
+		}
+	}
+}
+
+#endif /* CRYPTOPEN_X509_CERTIFICATE_HPP */
+
