5e593ac38cf025892c1a5d37260bcf066f1917f9
Julien Kauffmann <julien.kauffmann@freelan.org>
2011-03-24 17:21:54 +0100

    Added x509 specific code

--- samples/x509/x509.cpp
+++ samples/x509/x509.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <cryptoplus/x509/certificate.hpp>
+#include <cryptoplus/bio/bio_chain.hpp>
 #include <cryptoplus/error/error_strings.hpp>
 
 #include <iostream>
@@ -20,6 +21,23 @@ int main()
 	std::cout << "X509 sample" << std::endl;
 	std::cout << "===========" << std::endl;
 	std::cout << std::endl;
+
+	try
+	{
+		cryptoplus::x509::certificate certificate;
+
+		const char cn[] = "My common name";
+		certificate.subject().push_back("CN", MBSTRING_ASC, cn, sizeof(cn) - 1);
+
+		cryptoplus::bio::bio_chain bio_chain(BIO_new_fd(STDOUT_FILENO, BIO_NOCLOSE));
+		certificate.print(bio_chain.first());
+	}
+	catch (std::exception& ex)
+	{
+		std::cerr << "Error: " << ex.what() << std::endl;
+
+		return EXIT_FAILURE;
+	}
 
 	return EXIT_SUCCESS;
 }
