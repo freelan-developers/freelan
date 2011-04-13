7be7b9ba195ab118cfdaf7056a6c1d618bc20391
Julien Kauffmann <julien.kauffmann@freelan.org>
2011-04-13 17:11:42 +0200

    Added base files

---
+++ README.md
@@ -0,0 +1,20 @@
+libfmpp
+=======
+
+libfmpp provides a portable C++ implementation of the FreeLAN Message Part Protocol.
+
+It is mainly based on [boost::asio](http://www.boost.org/doc/libs/1_46_1/doc/html/boost_asio.html) and [libcryptoplus](https://github.com/ereOn/libcryptoplus).
+
+Licensing
+---------
+
+All code is licensed under the GPLv3. See gpl-3.0.txt.
+
+If you are interested in using this project under a different license, please [contact me](mailto:julien.kauffmann__AT__freelan.org). I don't bite and I probably won't charge (at least, not much).
+
+Does the project have a website ?
+---------------------------------
+
+Yes, it does. And [here](http://www.freelan.org/index.php?page=libfmpp) it is. 
+
+You may also find the [git repository](https://github.com/ereOn/libfmpp) on github.
---
+++ doc/doxygen-main.h
@@ -0,0 +1,30 @@
+/**
+ * \file doxygen-main.h
+ * \brief Documentation main page.
+ * \author Julien Kauffmann <julien.kauffmann@freelan.org>
+ * \date 2011
+ */
+
+/**
+ * \mainpage libfmpp Documentation
+ *
+ * \section section-intro Introduction
+ *
+ * This is the API of libfmpp, an OpenSource multiplatform C++ implementation of the FreeLAN Message Part protocol.
+ * 
+ * \section section-build Build instructions
+ * 
+ * To build and install libfmpp, see INSTALL file located in the source tree.
+ *  
+ * \section section-links Links
+ * 
+ * <ul>
+ * <li><a href="http://www.freelan.org">libfmpp website</a></li>
+ * <li><a href="https://github.com/ereOn/libfmpp">libfmpp project page</a></li>
+ * </ul>
+ * 
+ * \section section-license License
+ * 
+ * libfmpp is licensed under the <a href="http://www.gnu.org/licenses/gpl-3.0.html">GPLv3.0 license</a>.
+ */
+
