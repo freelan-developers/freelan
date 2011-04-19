71850db947cf1978e8d78aa0a3e3d51c50b29ac2
Julien Kauffmann <julien.kauffmann@freelan.org>
2011-04-19 22:48:23 +0200

    Added first sample

---
+++ samples/client/client.cpp
@@ -0,0 +1,12 @@
+/**
+ * \file client.cpp
+ * \author Julien Kauffmann <julien.kauffmann@freelan.org>
+ * \brief A simple client.
+ */
+
+#include <fscp/fscp.hpp>
+
+int main()
+{
+	return EXIT_SUCCESS;
+}
