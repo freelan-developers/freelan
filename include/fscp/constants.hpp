2d641721a923a5549bea2324b55ba3b36cb5413b
Julien Kauffmann <julien.kauffmann@freelan.org>
2011-05-03 11:08:21 +0200

    Changed the default AES algorithm

--- include/fscp/constants.hpp
+++ include/fscp/constants.hpp
@@ -73,7 +73,7 @@ namespace fscp
 	/**
 	 * \brief The cipher algorithm.
 	 */
-	const int CIPHER_ALGORITHM = NID_aes_256_cbc;
+	const int CIPHER_ALGORITHM = NID_aes_256_cfb128;
 
 	/**
 	 * \brief The message digest algorithm.
