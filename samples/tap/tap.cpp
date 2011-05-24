23e0125e8b275e1e2523f64b99b0ffd625e97991
Julien Kauffmann <julien.kauffmann@freelan.org>
2011-05-24 15:56:51 +0200

    Updated tap sample to test both read and write

--- samples/tap/tap.cpp
+++ samples/tap/tap.cpp
@@ -59,9 +59,22 @@ static bool register_signal_handlers()
 	return true;
 }
 
-void read_done(const boost::system::error_code& ec, size_t cnt)
+char my_buf[2048];
+
+void write_done(asiotap::tap_adapter& tap_adapter, const boost::system::error_code& ec, size_t cnt)
+{
+	(void)tap_adapter;
+	std::cout << "Write: " << cnt << " bytes. Error: " << ec << std::endl;
+}
+
+void read_done(asiotap::tap_adapter& tap_adapter, const boost::system::error_code& ec, size_t cnt)
 {
 	std::cout << "Read: " << cnt << " bytes. Error: " << ec << std::endl;
+
+	if (!ec)
+	{
+		tap_adapter.async_write(boost::asio::buffer(my_buf, cnt), boost::bind(&write_done, boost::ref(tap_adapter), _1, _2));
+	}
 }
 
 int main()
@@ -75,14 +88,12 @@ int main()
 
 	asiotap::tap_adapter tap_adapter(_io_service);
 
-	stop_function = boost::bind(&boost::asio::io_service::stop, &_io_service);
+	stop_function = boost::bind(&asiotap::tap_adapter::close, &tap_adapter);
 
 	tap_adapter.open();
 	tap_adapter.set_connected_state(true);
 
-	char buf[2048];
-
-	tap_adapter.async_read(boost::asio::buffer(buf, sizeof(buf)), &read_done);
+	tap_adapter.async_read(boost::asio::buffer(my_buf, sizeof(my_buf)), boost::bind(&read_done, boost::ref(tap_adapter), _1, _2));
 
 	_io_service.run();
 
