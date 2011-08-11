f0b98200dd34cd239bd27ed359c3fc376837f4e6
Julien Kauffmann <julien.kauffmann@freelan.org>
2011-08-11 10:03:31 +0200

    Added base builder classe

---
+++ include/asiotap/osi/builder.hpp
@@ -0,0 +1,101 @@
+/*
+ * libasiotap - A portable TAP adapter extension for Boost::ASIO.
+ * Copyright (C) 2010-2011 Julien Kauffmann <julien.kauffmann@freelan.org>
+ *
+ * This file is part of libasiotap.
+ *
+ * libasiotap is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * libasiotap is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * In addition, as a special exception, the copyright holders give
+ * permission to link the code of portions of this program with the
+ * OpenSSL library under certain conditions as described in each
+ * individual source file, and distribute linked combinations
+ * including the two.
+ * You must obey the GNU General Public License in all respects
+ * for all of the code used other than OpenSSL.  If you modify
+ * file(s) with this exception, you may extend this exception to your
+ * version of the file(s), but you are not obligated to do so.  If you
+ * do not wish to do so, delete this exception statement from your
+ * version.  If you delete this exception statement from all source
+ * files in the program, then also delete it here.
+ *
+ * If you intend to use libasiotap in a commercial software, please
+ * contact me : we may arrange this for a small fee or no fee at all,
+ * depending on the nature of your project.
+ */
+
+/**
+ * \file builder.hpp
+ * \author Julien KAUFFMANN <julien.kauffmann@freelan.org>
+ * \brief An OSI frame builder class.
+ */
+
+#ifndef ASIOTAP_OSI_BUILDER_HPP
+#define ASIOTAP_OSI_BUILDER_HPP
+
+#include <boost/asio.hpp>
+
+namespace asiotap
+{
+	namespace osi
+	{
+		/**
+		 * \brief A base const helper class.
+		 */
+		template <typename OSIFrameType>
+		class _base_builder
+		{
+			public:
+
+				/**
+				 * \brief The frame type.
+				 */
+				typedef OSIFrameType frame_type;
+
+				/**
+				 * \brief Get the underlying buffer.
+				 * \return The underlying buffer.
+				 */
+				boost::asio::mutable_buffer buffer() const;
+
+			protected:
+
+				/**
+				 * \brief Create a helper from a frame type structure.
+				 * \param buf The buffer to refer to.
+				 */
+				_base_builder(boost::asio::mutable_buffer buf);
+
+			private:
+
+				const boost::asio::const_buffer m_buf;
+		};
+		
+		template <typename OSIFrameType>
+		inline boost::asio::mutable_buffer _base_builder<OSIFrameType>::buffer() const
+		{
+			return m_buf;
+		}
+
+		template <typename OSIFrameType>
+		inline _base_builder<OSIFrameType>::_base_builder(boost::asio::mutable_buffer buf) :
+			m_buf(buf)
+		{
+		}
+	}
+}
+
+#endif /* ASIOTAP_OSI_BUILDER_HPP */
+
