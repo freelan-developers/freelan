72ad6267eca2e7bff5512045ebe201b870d6de3e
Julien Kauffmann <julien.kauffmann@freelan.org>
2011-08-29 17:22:31 +0200

    Added some sample code

--- samples/basic/basic.cpp
+++ samples/basic/basic.cpp
@@ -6,14 +6,34 @@
 
 #include <iconvplus/iconvplus.hpp>
 
+#include <cstring>
 #include <cstdlib>
 #include <iostream>
+#include <fstream>
 
 int main()
 {
 	try
 	{
-		iconvplus::iconv converter("utf-8", "latin1");
+		iconvplus::iconv converter("cp1252", "latin1");
+
+		std::ifstream ifs("material/latin1.txt");
+
+		if (ifs)
+		{
+			char buf[1024] = {};
+			char result_buf[1024] = {};
+
+			while (ifs.getline(buf, sizeof(buf)))
+			{
+				size_t result_len = converter.convert_all(buf, strlen(buf), result_buf, sizeof(result_buf));
+
+				std::cout << std::string(result_buf, result_len) << std::endl;
+			}
+		} else
+		{
+			std::cerr << "Unable to open the specified file." << std::endl;
+		}
 	}
 	catch (std::exception& ex)
 	{
