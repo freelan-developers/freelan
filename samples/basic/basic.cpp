5ecbb21723648131b7d5d32956ea91f4a5295132
Julien Kauffmann <julien.kauffmann@freelan.org>
2011-09-04 12:15:35 +0200

    Better basic sample code

--- samples/basic/basic.cpp
+++ samples/basic/basic.cpp
@@ -4,6 +4,7 @@
  * \brief A basic iconv sample.
  */
 
+#include <iconvplus/os.hpp>
 #include <iconvplus/iconvplus.hpp>
 
 #include <cstring>
@@ -15,27 +16,22 @@ int main()
 {
 	try
 	{
+		std::ifstream latin1_file("material/latin1.txt");
+		std::ifstream utf8_file("material/utf8.txt");
+
+#ifdef WINDOWS
 		iconvplus::iconv_instance latin1_to_cp1252("cp1252", "latin1");
+		iconvplus::iconv_instance utf8_to_wchar_t("wchar_t", "utf-8");
+
+		iconvplus::converter<char, char>(latin1_to_cp1252).convert(latin1_file, std::cout);
+		iconvplus::converter<char, wchar_t>(utf8_to_wchar_t).convert(utf8_file, std::wcout);
+#elif defined(UNIX)
 		iconvplus::iconv_instance latin1_to_utf8("utf-8", "latin1");
-		iconvplus::iconv_instance utf8_to_cp1252("cp1252", "utf-8");
-		iconvplus::iconv_instance utf8_to_latin1("latin1", "utf-8");
+		iconvplus::iconv_instance utf8_to_wchar_t("wchar_t", "utf-8");
 
-		std::ifstream latin1("material/latin1.txt");
-		std::ifstream utf8("material/utf8.txt");
-
-		iconvplus::converter cv;
-		
-		cv.convert(latin1_to_utf8, latin1, std::wcout);
-
- 		latin1.clear();
-		latin1.seekg(0);
-		cv.convert(latin1_to_cp1252, latin1, std::wcout);
-
-		cv.convert(utf8_to_latin1, utf8, std::wcout);
-
- 		utf8.clear();
-		utf8.seekg(0);
-		cv.convert(utf8_to_cp1252, utf8, std::wcout);
+		iconvplus::converter<char, char>(latin1_to_utf8).convert(latin1_file, std::cout);
+		iconvplus::converter<char, wchar_t>(utf8_to_wchar_t).convert(utf8_file, std::wcout);
+#endif
 	}
 	catch (std::exception& ex)
 	{
