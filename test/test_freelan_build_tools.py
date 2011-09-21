e0284ccf4b55b4a1b70bb1b1cae433a1406aa601
Julien Kauffmann <julien.kauffmann@freelan.org>
2011-09-21 17:31:40 +0200

    Added unit tests

---
+++ test/test_freelan_build_tools.py
@@ -0,0 +1,9 @@
+"""Unit tests for freelan.build_tools."""
+
+import unittest
+
+class Test(unittest.TestCase):
+    """Unit tests for freelan.build_tools."""
+
+if __name__ == '__main__':
+    unittest.main()
