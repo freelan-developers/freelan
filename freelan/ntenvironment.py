9026423c38c6715506694053ff5d7b33b146d996
Julien Kauffmann <julien.kauffmann@freelan.org>
2011-09-27 10:50:19 +0200

    Removed paths from build system: these have to be taken from the environment

--- freelan/ntenvironment.py
+++ freelan/ntenvironment.py
@@ -52,97 +52,22 @@ class NtEnvironment(BaseEnvironment):
             self['CXXFLAGS'].append('/EHsc')
 
         self['ARGUMENTS'].setdefault('prefix', r'C:\mingw')
-        self['ARGUMENTS'].setdefault('openssl_path', r'C:\openssl')
-        self['ARGUMENTS'].setdefault('boost_path', r'C:\boost')
-        self['ARGUMENTS'].setdefault('boost_version', '1_47')
-
-        if self.toolset == 'mingw':
-            self['ARGUMENTS'].setdefault('boost_suffix', 'mgw45-mt')
-        else:
-            self['ARGUMENTS'].setdefault('boost_suffix', 'vc100-mt')
-
-        self['ARGUMENTS'].setdefault('cryptoplus_path', r'C:\mingw')
-        self['ARGUMENTS'].setdefault('asiotap_path', r'C:\mingw')
-        self['ARGUMENTS'].setdefault('iconvplus_path', r'C:\mingw')
-        self['ARGUMENTS'].setdefault('fscp_path', r'C:\mingw')
-        self['ARGUMENTS'].setdefault('freelan_path', r'C:\mingw')
 
     def FreelanLibrary(self, target_dir, name, major, minor, include_path, source_files, libraries):
         """Build a library."""
 
-        kw = {
-            'CPPPATH': [include_path]
+        env = {
+            'CPPPATH': [include_path],
+            'LIBS': libraries
         }
 
-        for library in libraries:
-            self.__update_environment_from_library(kw, library)
-
-        for key, value in kw.items():
+        for key, value in env.items():
             if isinstance(value, list):
                 if key in self:
-                    kw[key] += self[key]
+                    env[key] += self[key]
 
-                kw[key][:] = tools.unique(kw[key])
-
-        objects = self.Object(source = source_files, **kw)
-        shared_library = self.SharedLibrary(os.path.join(target_dir, name), objects, **kw)
-        static_library = self.StaticLibrary(os.path.join(target_dir, name + '_static'), objects, **kw)
+        objects = self.Object(source = source_files, **env)
+        shared_library = self.SharedLibrary(os.path.join(target_dir, name), objects, **env)
+        static_library = self.StaticLibrary(os.path.join(target_dir, name + '_static'), objects, **env)
 
         return shared_library + static_library
-
-    def __update_environment_from_library(self, env, library):
-        """Update the environment according to the specified library."""
-
-        if library.startswith('openssl_'):
-            env.setdefault('CPPPATH', []).append(os.path.join(self['ARGUMENTS']['openssl_path'], 'include'))
-            env.setdefault('LIBPATH', []).append(os.path.join(self['ARGUMENTS']['openssl_path'], 'lib'))
-
-        if library == 'openssl_ssl':
-            if self.toolset == 'mingw':
-                env.setdefault('LIBS', []).append('ssl')
-            else:
-                env.setdefault('LIBS', []).append('ssleay32')
-
-        if library == 'openssl_crypto':
-            if self.toolset == 'mingw':
-                env.setdefault('LIBS', []).append('crypto')
-            else:
-                env.setdefault('LIBS', []).append('libeay32')
-            env.setdefault('LIBS', []).append('gdi32')
-
-        if library.startswith('boost'):
-            if self.toolset == 'mingw':
-                env.setdefault('CXXFLAGS', []).append('-isystem' + os.path.join(self['ARGUMENTS']['boost_path'], 'include', 'boost-' + self['ARGUMENTS']['boost_version']))
-            else:
-                env.setdefault('CXXFLAGS', []).append('-DBOOST_ALL_NO_LIB=1')
-                env.setdefault('CPPPATH', []).append(os.path.join(self['ARGUMENTS']['boost_path'], 'include', 'boost-' + self['ARGUMENTS']['boost_version']))
-
-            if library.startswith('boost_'):
-                env.setdefault('LIBPATH', []).append(os.path.join(self['ARGUMENTS']['boost_path'], 'lib'))
-                env.setdefault('LIBS', []).append(library + '-%s-%s' % (self['ARGUMENTS']['boost_suffix'], self['ARGUMENTS']['boost_version']))
-
-        if library.startswith('cryptoplus'):
-            env.setdefault('LIBS', []).append(library)
-            env.setdefault('CPPPATH', []).append(os.path.join(self['ARGUMENTS']['cryptoplus_path'], 'include'))
-            env.setdefault('LIBPATH', []).append(os.path.join(self['ARGUMENTS']['cryptoplus_path'], 'lib'))
-
-        if library.startswith('asiotap'):
-            env.setdefault('LIBS', []).append(library)
-            env.setdefault('CPPPATH', []).append(os.path.join(self['ARGUMENTS']['asiotap_path'], 'include'))
-            env.setdefault('LIBPATH', []).append(os.path.join(self['ARGUMENTS']['asiotap_path'], 'lib'))
-
-        if library.startswith('fscp'):
-            env.setdefault('LIBS', []).append(library)
-            env.setdefault('CPPPATH', []).append(os.path.join(self['ARGUMENTS']['fscp_path'], 'include'))
-            env.setdefault('LIBPATH', []).append(os.path.join(self['ARGUMENTS']['fscp_path'], 'lib'))
-
-        if library.startswith('iconvplus'):
-            env.setdefault('LIBS', []).append(library)
-            env.setdefault('CPPPATH', []).append(os.path.join(self['ARGUMENTS']['iconvplus_path'], 'include'))
-            env.setdefault('LIBPATH', []).append(os.path.join(self['ARGUMENTS']['iconvplus_path'], 'lib'))
-
-        if library.startswith('freelan'):
-            env.setdefault('LIBS', []).append(library)
-            env.setdefault('CPPPATH', []).append(os.path.join(self['ARGUMENTS']['freelan_path'], 'include'))
-            env.setdefault('LIBPATH', []).append(os.path.join(self['ARGUMENTS']['freelan_path'], 'lib'))
-
--- freelan/posixenvironment.py
+++ freelan/posixenvironment.py
@@ -50,79 +50,24 @@ class PosixEnvironment(BaseEnvironment):
                 self['LINKFLAGS'].append('-m64')
 
         self['ARGUMENTS'].setdefault('prefix', '/usr/local')
-        self['ARGUMENTS'].setdefault('openssl_path', '/usr')
-        self['ARGUMENTS'].setdefault('boost_path', '/usr')
-        self['ARGUMENTS'].setdefault('cryptoplus_path', '/usr/local')
-        self['ARGUMENTS'].setdefault('asiotap_path', '/usr/local')
-        self['ARGUMENTS'].setdefault('iconvplus_path', '/usr/local')
-        self['ARGUMENTS'].setdefault('fscp_path', '/usr/local')
-        self['ARGUMENTS'].setdefault('freelan_path', '/usr/local')
 
     def FreelanLibrary(self, target_dir, name, major, minor, include_path, source_files, libraries):
         """Build a library."""
 
-        kw = {
+        env = {
             'CPPPATH': [include_path],
-            'SHLINKFLAGS': ['-Wl,-soname,lib%s.so.%s' % (name, major)]
+            'SHLINKFLAGS': ['-Wl,-soname,lib%s.so.%s' % (name, major)],
+            'LIBS': libraries
         }
 
-        for library in libraries:
-            self.__update_environment_from_library(kw, library)
-
-        for key, value in kw.items():
+        # We add values to existing ones
+        for key, value in env.items():
             if isinstance(value, list):
                 if key in self:
-                    kw[key] += self[key]
+                    env[key] += self[key]
 
-                kw[key][:] = tools.unique(kw[key])
-
-        shared_library = self.SharedLibrary(os.path.join(target_dir, name), source_files, **kw)
-        static_library = self.StaticLibrary(os.path.join(target_dir, name + '_static'), source_files, **kw)
+        shared_library = self.SharedLibrary(os.path.join(target_dir, name), source_files, **env)
+        static_library = self.StaticLibrary(os.path.join(target_dir, name + '_static'), source_files, **env)
         versioned_shared_library = self.Command(os.path.join(target_dir, 'lib%s.so.%s.%s' % (name, major, minor)), shared_library, SCons.Script.Copy("$TARGET", "$SOURCE"))
 
         return shared_library + static_library + versioned_shared_library
-
-    def __update_environment_from_library(self, env, library):
-        """Update the environment according to the specified library."""
-
-        if library.startswith('openssl_'):
-            env.setdefault('CPPPATH', []).append(os.path.join(self['ARGUMENTS']['openssl_path'], 'include'))
-            env.setdefault('LIBPATH', []).append(os.path.join(self['ARGUMENTS']['openssl_path'], 'lib'))
-
-        if library == 'openssl_ssl':
-            env.setdefault('LIBS', []).append('ssl')
-
-        if library == 'openssl_crypto':
-            env.setdefault('LIBS', []).append('crypto')
-
-        if library.startswith('boost'):
-            env.setdefault('CXXFLAGS', []).append('-isystem' + os.path.join(self['ARGUMENTS']['boost_path'], 'include'))
-
-            if library.startswith('boost_'):
-                env.setdefault('LIBS', []).append(library)
-
-        if library.startswith('cryptoplus'):
-            env.setdefault('LIBS', []).append(library)
-            env.setdefault('CPPPATH', []).append(os.path.join(self['ARGUMENTS']['cryptoplus_path'], 'include'))
-            env.setdefault('LIBPATH', []).append(os.path.join(self['ARGUMENTS']['cryptoplus_path'], 'lib'))
-
-        if library.startswith('asiotap'):
-            env.setdefault('LIBS', []).append(library)
-            env.setdefault('CPPPATH', []).append(os.path.join(self['ARGUMENTS']['asiotap_path'], 'include'))
-            env.setdefault('LIBPATH', []).append(os.path.join(self['ARGUMENTS']['asiotap_path'], 'lib'))
-
-        if library.startswith('fscp'):
-            env.setdefault('LIBS', []).append(library)
-            env.setdefault('CPPPATH', []).append(os.path.join(self['ARGUMENTS']['fscp_path'], 'include'))
-            env.setdefault('LIBPATH', []).append(os.path.join(self['ARGUMENTS']['fscp_path'], 'lib'))
-
-        if library.startswith('iconvplus'):
-            env.setdefault('LIBS', []).append(library)
-            env.setdefault('CPPPATH', []).append(os.path.join(self['ARGUMENTS']['iconvplus_path'], 'include'))
-            env.setdefault('LIBPATH', []).append(os.path.join(self['ARGUMENTS']['iconvplus_path'], 'lib'))
-
-        if library.startswith('freelan'):
-            env.setdefault('LIBS', []).append(library)
-            env.setdefault('CPPPATH', []).append(os.path.join(self['ARGUMENTS']['freelan_path'], 'include'))
-            env.setdefault('LIBPATH', []).append(os.path.join(self['ARGUMENTS']['freelan_path'], 'lib'))
-
