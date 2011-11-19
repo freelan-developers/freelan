caa5b11b84b310cc55de31613a25bb04d3fc1653
Julien Kauffmann <julien.kauffmann@freelan.org>
2011-11-19 12:44:40 +0100

    Added support for NSIS files

--- freelan/nsis.py
+++ freelan/nsis.py
@@ -1,2 +1,95 @@
 """A NSIS builder for SCons"""
 
+import re
+
+def uncomment(text):
+    """Remove all NSIS comments from the specified text"""
+
+    def replacer(match):
+        s = match.group(0)
+        if s.startswith(';') or s.startswith('#'):
+            return ''
+        else:
+            return s
+
+    pattern = '#.*?$|;.*?$|"[^"]*"|\'[^"]*\''
+
+    return re.sub(re.compile(pattern, re.DOTALL | re.MULTILINE), replacer, text)
+
+def parse_include(line):
+    """Check if the specified line contains an !include directive"""
+
+    pattern = "!include(?:\s+)(?:[\"']?([\w.]*)[\"']?)"
+
+    match = re.match(pattern, line)
+
+    if match:
+        return match.group(1)
+
+def replace_defines(text, defines):
+    """Replace defines in the specified text."""
+
+    for define in defines.items():
+        text = text.replace('${%s}' % define[0], define[1])
+
+    return text
+
+def nsis_scanner(node, env, path, source_dir = None):
+    """The scanner"""
+
+    content = replace_defines(node.get_contents(), env['NSIS_DEFINES'])
+
+    result = []
+
+    for line in uncomment(content).split('\n'):
+        include = parse_include(line)
+
+        if include: result.append(env.File(include))
+
+    return result
+
+def nsis_emitter(source, target, env):
+    """The emitter"""
+
+    return (target, source)
+
+def nsis_generator(source, target, env, for_signature):
+    """The generator"""
+
+    return ''
+    result = '%s %s %s "/XOutFile %s" -- %s' % \
+            (
+                env['MAKENSIS'],
+                ' '.join(env['NSIS_FLAGS']),
+                ' '.join(['"/D%s=%s"' % (x[0], x[1]) for x in env['NSIS_DEFINES'].items()]),
+                target[0],
+                source[0]
+            )
+
+    return result
+
+def generate(env):
+    env.Append(MAKENSIS = 'makensis')
+    env.Append(NSIS_FLAGS = []);
+    env.Append(NSIS_DEFINES = {});
+
+    import SCons.Scanner
+
+    env.Append(SCANNERS = SCons.Scanner.Scanner(
+        function = nsis_scanner,
+        skeys = ['.nsi', '.nsh']
+    ))
+
+    import SCons.Builder
+
+    nsis_builder = SCons.Builder.Builder(
+        generator = nsis_generator,
+        emitter = nsis_emitter,
+        suffix = '.exe',
+        src_suffix = '.nsi'
+    )
+
+    env.Append(BUILDERS = {'NSIS': nsis_builder})
+
+def exists(env):
+    return env.Detect(env['MAKENSIS'])
