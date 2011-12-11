3e9c7641bb614ce0fbdab48f86e8bb2d571c65e5
Julien Kauffmann <julien.kauffmann@freelan.org>
2011-12-11 22:19:09 +0100

    Added more test cases to the endpoint sample

--- samples/endpoint/endpoint.cpp
+++ samples/endpoint/endpoint.cpp
@@ -38,6 +38,8 @@ int main()
 		std::cout << boost::lexical_cast<freelan::endpoint>("127.1:45") << std::endl;
 		std::cout << boost::lexical_cast<freelan::endpoint>("[3::4:5ae]:78") << std::endl;
 		std::cout << boost::lexical_cast<freelan::endpoint>("some.host.com:service") << std::endl;
+		std::cout << boost::lexical_cast<freelan::ipv4_network_address>("9.0.0.1/24") << std::endl;
+		std::cout << boost::lexical_cast<freelan::ipv6_network_address>("fe80::1/10") << std::endl;
 	}
 	catch (std::exception& ex)
 	{
