38c6ca74c8bdbd6b809e8da5085cc98a0a3acae7
Julien Kauffmann <julien.kauffmann@freelan.org>
2011-12-11 19:00:12 +0100

    Updated endpoint sample to test for ip_network_address classes

--- samples/endpoint/endpoint.cpp
+++ samples/endpoint/endpoint.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <freelan/endpoint.hpp>
+#include <freelan/ip_network_address.hpp>
 
 #include <cstdlib>
 #include <csignal>
@@ -19,11 +20,15 @@ int main()
 		freelan::endpoint ep1 = freelan::hostname_endpoint("localhost", "1234");
 		freelan::endpoint ep2 = freelan::ipv4_endpoint(boost::asio::ip::address_v4::loopback(), 1234);
 		freelan::endpoint ep3 = freelan::ipv6_endpoint(boost::asio::ip::address_v6::loopback(), 1234);
+		freelan::ipv4_network_address na1 = freelan::ipv4_network_address(boost::asio::ip::address_v4::loopback(), 24);
+		freelan::ipv6_network_address na2 = freelan::ipv6_network_address(boost::asio::ip::address_v6::loopback(), 64);
 
 		// Output
 		std::cout << "ep1: " << ep1 << std::endl;
 		std::cout << "ep2: " << ep2 << std::endl;
 		std::cout << "ep3: " << ep3 << std::endl;
+		std::cout << "na1: " << na1 << std::endl;
+		std::cout << "na2: " << na2 << std::endl;
 
 		// Comparison
 		std::cout << "ep1 == ep2: " << (ep1 == ep2) << std::endl;
