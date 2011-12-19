031835393c892b35f29841d78138c97e19b2e23f
Julien Kauffmann <julien.kauffmann@freelan.org>
2011-12-19 16:15:00 +0100

    Better design for scripts

--- scripts/generate_intermediate_ca.sh
+++ scripts/generate_intermediate_ca.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if [ "$#" -ne 2 ]; then
-	echo "Syntax: $0 <ca_path> <root_ca_path>" >&2
+if [ "$#" -ne 3 ]; then
+	echo "Syntax: $0 <ca_path> <root_ca_path> <name>" >&2
 	echo >&2
 	echo "Generate a certificate for the intermediate CA at the specified path." >&2
 	exit 1
@@ -9,6 +9,7 @@ fi
 
 CA_PATH=`readlink -f $1`
 ROOT_CA_PATH=`readlink -f $2`
+NAME=$3
 
 if [ ! -d "$CA_PATH" ]; then
 	echo "Error: Directory \"$CA_PATH\" does not exists." >&2
@@ -25,9 +26,6 @@ openssl genrsa -des3 -out $CA_PATH/key/ca.key 4096 -config $CA_PATH/ca.cnf
 
 echo "Generating CA certificate request:"
 openssl req -new -sha1 -key $CA_PATH/key/ca.key -out $CA_PATH/crt/ca.csr -config $CA_PATH/ca.cnf
-
-echo "Please enter a disctintive name for the intermediate certificate authority:"
-read NAME
 
 echo "Signing certificate with root CA:"
 openssl ca -extensions v3_ca -out $ROOT_CA_PATH/crt/$NAME.crt -in $CA_PATH/crt/ca.csr -config $ROOT_CA_PATH/ca.cnf
