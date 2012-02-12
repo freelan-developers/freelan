73ebda3a7330dc07358818dad2e139bd4aabfbb3
Julien Kauffmann <julien.kauffmann@freelan.org>
2012-02-12 16:58:57 +0100

    Updated build script

--- installers/linux/libcryptoplus/build.sh
+++ installers/linux/libcryptoplus/build.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
 
 PROJECT_NAME=libcryptoplus
-PROJECT_VERSION=master
-GIT_TAG=${PROJECT_VERSION}
+PROJECT_VERSION=1.0
+#GIT_TAG=${PROJECT_VERSION}
+GIT_TAG=master
+MAINTAINER_EMAIL=julien.kauffmann@freelan.org
 
-git archive --remote ../../../${PROJECT_NAME} --format tar --prefix ${PROJECT_NAME}/ ${GIT_TAG} | gzip > ${PROJECT_NAME}.tar.gz
+### DO NOT CHANGE ANYTHING BELOW THIS LINE ! ###
+
+# Extract the git archive
+git archive --remote ../../../${PROJECT_NAME} --format tar --prefix ${PROJECT_NAME}-${PROJECT_VERSION}/ ${GIT_TAG} | gzip > ${PROJECT_NAME}-${PROJECT_VERSION}.tar.gz
+
+# We extract the source
+tar zxf ${PROJECT_NAME}-${PROJECT_VERSION}.tar.gz
+
+# We rename it as a orig file
+mv ${PROJECT_NAME}-${PROJECT_VERSION}.tar.gz ${PROJECT_NAME}_${PROJECT_VERSION}.orig.tar.gz
+
+# We run dh_make
+cd ${PROJECT_NAME}-${PROJECT_VERSION}
+dh_make -l -e ${MAINTAINER_EMAIL} -c gpl3
