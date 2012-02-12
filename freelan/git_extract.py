fce34e7e147e9c201190cc06f2cfa743d2c35470
Julien Kauffmann <julien.kauffmann@freelan.org>
2012-02-12 18:20:47 +0100

    Revert "Added support for several Git extraction formats"
    This reverts commit 507f5bd39442287e2508a10c7bfddc2d7ed2a50a.

--- freelan/git_extract.py
+++ freelan/git_extract.py
@@ -6,21 +6,13 @@ import os
 def git_extract_emitter(target, source, env):
     """The emitter"""
 
-    if env['GIT_EXTRACT_FORMAT'] == 'gzip':
-        suffix = '.tar.gz'
-    elif env['GIT_EXTRACT_FORMAT'] == 'tar':
-        suffix = '.tar'
-    else:
-        suffix = None
-
-    if suffix:
-        if os.path.isdir(str(target[0])):
-            target = [
-                os.path.join(
-                    str(target[0]),
-                    os.path.basename(str(source[0])) + '-' + env['GIT_EXTRACT_REF'] + suffix
-                )
-            ]
+    if os.path.isdir(str(target[0])):
+        target = [
+            os.path.join(
+                str(target[0]),
+                os.path.basename(str(source[0])) + '-' + env['GIT_EXTRACT_REF'] + env['BUILDERS']['GitExtract'].suffix
+            )
+        ]
 
     return target, source
 
@@ -33,41 +25,29 @@ def git_extract_generator(target, source, env, for_signature):
     if len(source) != 1:
         raise SCons.Errors.BuildError(target, 'More than one source specified')
 
-    if env['GIT_EXTRACT_FORMAT'] == 'gzip':
-        prefix = os.path.basename(str(target[0])).replace('.tar.gz', '') + '/'
-        format_str = '%(git)s archive --remote %(source)s --prefix %(prefix)s %(ref)s --format tar | %(zip)s > %(target)s'
-    elif env['GIT_EXTRACT_FORMAT'] == 'tar':
-        prefix = os.path.basename(str(target[0])).replace('.tar', '') + '/'
-        format_str = '%(git)s archive --remote %(source)s --prefix %(prefix)s %(ref)s --format tar > %(target)s'
-    elif env['GIT_EXTRACT_FORMAT'] == 'folder':
-        prefix = os.path.basename(str(target[0])) + '/'
-        format_str = '%(git)s archive --remote %(source)s --prefix %(prefix)s %(ref)s --format tar > %(target)s.tar && %(tar)s xvf %(target)s.tar && rm %(target)s.tar'
-    else:
-        raise SCons.Errors.BuildError(target, 'Unknown format specified (%s)' % env['GIT_EXTRACT_FORMAT'])
+    result = '%s archive --remote %s --prefix %s %s --format tar | %s > %s' % \
+            (
+                env['GIT_EXTRACT_TOOL'],
+                str(source[0]),
+                os.path.basename(str(target[0])).replace(env['BUILDERS']['GitExtract'].suffix, '') + '/',
+                env['GIT_EXTRACT_REF'],
+                env['GIT_EXTRACT_ZIP_TOOL'],
+                str(target[0]),
+            )
 
-    return format_str % \
-            {
-                'git': env['GIT_EXTRACT_TOOL'],
-                'source': str(source[0]),
-                'prefix': prefix,
-                'ref': env['GIT_EXTRACT_REF'],
-                'zip': env['GIT_EXTRACT_ZIP_TOOL'],
-                'target': str(target[0]),
-                'tar': env['GIT_EXTRACT_TAR_TOOL'],
-            }
+    return result
 
 def generate(env):
     env.Append(GIT_EXTRACT_TOOL = 'git')
-    env.Append(GIT_EXTRACT_TAR_TOOL = 'tar')
     env.Append(GIT_EXTRACT_ZIP_TOOL = 'gzip')
     env.Append(GIT_EXTRACT_REF = 'master')
-    env.Append(GIT_EXTRACT_FORMAT = 'gzip')
 
     import SCons.Builder
 
     git_extract_builder = SCons.Builder.Builder(
         generator = git_extract_generator,
         emitter = git_extract_emitter,
+        suffix = '.tar.gz',
     )
 
     env.Append(BUILDERS = {'GitExtract': git_extract_builder})
