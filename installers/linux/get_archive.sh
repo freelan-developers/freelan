502814a140851acd83d78dfdb725f88b3dde1242
Julien Kauffmann <julien.kauffmann@freelan.org>
2012-02-16 23:33:44 +0100

    Updated get_archive script

--- installers/linux/get_archive.sh
+++ installers/linux/get_archive.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
-DIR=$(pwd)
-
-if [ $# -gt 0 ]
+if [ $# -ne 2 ]
 then
-    if [ -d "$1" ]
-    then
-        DIR=$(cd $1 && pwd)
-    else
-        echo "\"$1\" does not exist. Using current directory instead." >&2
-    fi
+    echo "Syntax is: $0 <project> <version>" >&2
+    exit 2
 fi
 
-BASENAME=$(basename ${DIR})
-PROJECT_NAME=$(echo ${BASENAME} | cut -f1 -d_)
-PROJECT_VERSION=$(echo ${BASENAME} | cut -f2 -d_)
+PROJECT_NAME=$1
+PROJECT_VERSION=$2
 ROOT=$(git rev-parse --show-toplevel)
 
 echo "Getting ${PROJECT_NAME} version ${PROJECT_VERSION}..."
 
-git archive --remote ${ROOT}/${PROJECT_NAME} --format tar --prefix ${PROJECT_NAME}-${PROJECT_VERSION}/ ${PROJECT_VERSION} | gzip > ${DIR}/${PROJECT_NAME}_${PROJECT_VERSION}.orig.tar.gz
+git archive --remote ${ROOT}/${PROJECT_NAME} --format tar --prefix ${PROJECT_NAME}-${PROJECT_VERSION}/ ${PROJECT_VERSION} | gzip > ${PROJECT_NAME}_${PROJECT_VERSION}.orig.tar.gz
+tar zxf ${PROJECT_NAME}_${PROJECT_VERSION}.orig.tar.gz
