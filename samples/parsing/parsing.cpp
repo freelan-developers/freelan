49ae4079e3ce9210d18643061592ad5124d34b69
Julien Kauffmann <juka@4js.com>
2012-10-10 13:33:35 +0200

    Added basic sample

---
+++ samples/parsing/parsing.cpp
@@ -0,0 +1,19 @@
+/**
+ * \file parsing.cpp
+ * \author Julien Kauffmann <julien.kauffmann@freelan.org>
+ * \brief A parsing sample file.
+ */
+
+#include <kfather/parser.hpp>
+
+#include <cstdlib>
+#include <iostream>
+
+int main()
+{
+	std::cout << "Parsing sample" << std::endl;
+	std::cout << "=============" << std::endl;
+	std::cout << std::endl;
+
+	return EXIT_SUCCESS;
+}
