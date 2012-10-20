1e24373897eb9cb0ea84ab035d390c5f53e9ca0c
Julien Kauffmann <julien.kauffmann@freelan.org>
2012-10-20 16:53:11 +0200

    Made the parsing sample do a lot more things

--- samples/parsing/parsing.cpp
+++ samples/parsing/parsing.cpp
@@ -6,11 +6,33 @@
 
 #include <kfather/kfather.hpp>
 #include <kfather/parser.hpp>
+#include <kfather/formatter.hpp>
+#include <kfather/serializer.hpp>
 
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
-#include <streambuf>
+
+void parse_sample(json::parser& parser, const std::string& sample)
+{
+	std::cout << "Processing sample \"" << sample << "\"..." << std::endl;
+
+	std::ifstream file(("json/" + sample + ".json").c_str());
+
+	while (file)
+	{
+		if (parser.parse(file))
+		{
+			std::cout << std::endl;
+		}
+		else
+		{
+			std::cout << std::endl;
+
+			break;
+		}
+	}
+}
 
 int main()
 {
@@ -18,25 +40,15 @@ int main()
 	std::cout << "=============" << std::endl;
 	std::cout << std::endl;
 
-	std::ifstream file("json/glossary.json");
-
 	json::parser parser;
+	json::formatter formatter(parser, std::cout);
 
-	std::string str = std::string(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>());
-
-	std::string::size_type error_pos = 0;
-
-	if (!parser.parse(str, &error_pos))
-	{
-		if (error_pos < str.size())
-		{
-			std::cerr << "Parsing error at character " << error_pos << " ('" << str[error_pos] << "')" << std::endl;
-		}
-		else
-		{
-			std::cerr << "Parsing error at the end of the string." << std::endl;
-		}
-	}
+	parse_sample(parser, "constants");
+	parse_sample(parser, "numbers");
+	parse_sample(parser, "strings");
+	parse_sample(parser, "arrays");
+	parse_sample(parser, "objects");
+	parse_sample(parser, "glossary");
 
 	return EXIT_SUCCESS;
 }
