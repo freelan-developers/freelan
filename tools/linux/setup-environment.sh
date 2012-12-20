2d6176aa5cd09c894fec81520f7d5625fdfa8d05
Julien Kauffmann <julien.kauffmann@freelan.org>
2012-12-20 13:29:41 +0100

    Added a build environment script for Linux

---
+++ tools/linux/setup-environment.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+# A script sets up a build environment for freelan.
+#
+# This file is versioned so you probably should copy it first before modifying
+# it if you intend to.
+
+# Get the root directory
+export FREELAN_ALL_ROOT=$(readlink -f `dirname $0`/../..)
+
+# BUILD_ROOT is a directory which contains include, lib and bin sub-directories.
+export FREELAN_BUILD_ROOT="${FREELAN_ALL_ROOT}/build"
+
+# Create the subdirectories
+for SUBDIR in include lib etc bin; do
+	mkdir -p ${FREELAN_BUILD_ROOT}/${SUBDIR}
+done
+
+# This allows me to type ldconfig as a non-root user.
+alias ldconfig="ldconfig -r ${FREELAN_BUILD_ROOT}"
+
+# Freelan default installation goes into the build root
+export FREELAN_INSTALL_PREFIX="${FREELAN_BUILD_ROOT}"
+
+export CPLUS_INCLUDE_PATH="${FREELAN_INSTALL_PREFIX}/include:${CPLUS_INCLUDE_PATH}"
+export C_INCLUDE_PATH="${FREELAN_INSTALL_PREFIX}/include:${C_INCLUDE_PATH}"
+export LIBRARY_PATH="${FREELAN_INSTALL_PREFIX}/lib:${LIBRARY_PATH}"
+export LD_LIBRARY_PATH="${FREELAN_INSTALL_PREFIX}/lib:${LD_LIBRARY_PATH}"
+export PATH="${FREELAN_INSTALL_PREFIX}/bin:${PATH}"
+
+# Adds freelan-buildtools to the PYTHONPATH
+export PYTHONPATH="${FREELAN_ALL_ROOT}/freelan-buildtools:$PYTHONPATH"
+
+# Make the environment history local
+export HISTFILE="${FREELAN_ALL_ROOT}/.bash_history"
+
+# This adds a prefix to the PS1
+# To enable the feature, modify your PS1 so that it starts with ${PS1_PREFIX}.
+export PS1_PREFIX="[freelan] "
+
+# We start a shell that inherits from this environment
+echo "Setting-up freelan build environment"
+echo
+echo "Root is: ${FREELAN_ALL_ROOT}"
+echo "Build root is: ${FREELAN_BUILD_ROOT}"
+echo
+echo "Type Ctrl+D or exit to leave the build environment."
+
+$SHELL
