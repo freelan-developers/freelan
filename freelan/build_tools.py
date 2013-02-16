a638e470220814888f135de952ad658fe00d7ce0
Julien Kauffmann <julien.kauffmann@freelan.org>
2013-02-16 15:37:41 +0100

    Removed unused file

--- freelan/build_tools.py
+++
@@ -1,7 +0,0 @@
-"""A set of tools to ease FreeLAN related libraries."""
-
-# This module is deprecated !
-#
-# Use 'freelan.buildtools' instead.
-
-from buildtools import *
--- test/test_freelan_build_tools.py
+++
@@ -1,9 +0,0 @@
-"""Unit tests for freelan.build_tools."""
-
-import unittest
-
-class Test(unittest.TestCase):
-    """Unit tests for freelan.build_tools."""
-
-if __name__ == '__main__':
-    unittest.main()
