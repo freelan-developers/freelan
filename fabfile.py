15a78eb48df2986f421ab5b66920585f6d325b48
Julien Kauffmann <julien.kauffmann@freelan.org>
2013-05-06 20:22:15 +0200

    Added a fabric file to generate archives

---
+++ fabfile.py
@@ -0,0 +1,28 @@
+"""
+The Fabric file.
+"""
+
+ARCHIVES = {
+    'libkfather': [
+        '1.0',
+    ],
+}
+ARCHIVES_OUTPUT_DIR = 'archives'
+
+from fabric.api import *
+
+def archive():
+    """
+    Make archives of the git repositories.
+    """
+
+    local('mkdir -p %s' % ARCHIVES_OUTPUT_DIR)
+
+    for repository, tags in ARCHIVES.items():
+        for tag in tags:
+            with lcd(repository):
+                local('git archive %(tag)s | gzip > ../%(output_dir)s/%(repository)s_%(tag)s.orig.tar.gz' % {
+                    'repository': repository,
+                    'tag': tag,
+                    'output_dir': ARCHIVES_OUTPUT_DIR,
+                })
