6e35c9c715194875328e0997e6b741ed618aa212
Julien Kauffmann <julien.kauffmann@freelan.org>
2013-05-07 18:45:43 +0200

    Added sources fetching support

--- packaging/debian/fabfile.py
+++ packaging/debian/fabfile.py
@@ -4,12 +4,15 @@ The Fabric file.
 
 import os
 
-ARCHIVES = {
-    'libkfather': [
-        '1.0',
-    ],
+REPOSITORIES = {
+    'libkfather': {
+        'tags': [
+            '1.0',
+        ],
+    },
 }
 ARCHIVES_OUTPUT_DIR = 'archives'
+SOURCES_DIR = 'sources'
 
 from fabric.api import *
 
@@ -22,13 +25,38 @@ def archives():
 
     local('mkdir -p %s' % archives_path)
 
-    for repository, tags in ARCHIVES.items():
+    for repository, attributes in REPOSITORIES.items():
         repository_path = os.path.abspath(os.path.join('..', '..', repository))
 
-        for tag in tags:
+        for tag in attributes.get('tags', []):
             with lcd(repository_path):
                 local('git archive %(tag)s | gzip > %(output_dir)s/%(repository)s_%(tag)s.orig.tar.gz' % {
                     'repository': repository,
                     'tag': tag,
                     'output_dir': archives_path,
                 })
+
+def sources(override=False):
+    """
+    Generate the source packages.
+    """
+
+    sources_path = os.path.abspath(os.path.join(SOURCES_DIR))
+    vcs_uri_pattern = 'git@github.com:freelan-developers/%(repository)s.debian.git'
+
+    local('mkdir -p %s' % sources_path)
+
+    for repository in REPOSITORIES:
+
+        vcs_uri = vcs_uri_pattern % {
+            'repository': repository,
+        }
+
+        with lcd(sources_path):
+            if override:
+                local('rm -rf %s.debian' % repository)
+
+            local('[ -d %(repository)s.debian ] || git clone %(uri)s' % {
+                'uri': vcs_uri,
+                'repository': repository,
+            })
