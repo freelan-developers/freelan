7c6ac5be7524f95b4e4353ebd6ec9a6dd98dc8d1
Julien Kauffmann <julien.kauffmann@freelan.org>
2013-11-24 17:26:10 +0100

    Read the local routes from the configuration

--- src/core.cpp
+++ src/core.cpp
@@ -752,9 +752,7 @@ namespace freelan
 			else
 			{
 				// Registers the router port.
-				//
-				//TODO: Get the routes somewhere...
-				routes_type local_routes;
+				const routes_type& local_routes = m_configuration.tap_adapter.local_ip_routes;
 
 				m_tap_adapter_router_port = boost::make_shared<tap_adapter_router_port>(boost::ref(*m_tap_adapter), local_routes);
 				m_router.register_port(m_tap_adapter_router_port, TAP_ADAPTERS_GROUP);
