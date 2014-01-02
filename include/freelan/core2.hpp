e2279f55782d6244b9597ddad994c20dc94a819a
Julien Kauffmann <julien.kauffmann@freelan.org>
2014-01-02 10:27:06 +0100

    Added base files for rewrite

---
+++ include/freelan/core2.hpp
@@ -0,0 +1,76 @@
+/*
+ * libfreelan - A C++ library to establish peer-to-peer virtual private
+ * networks.
+ * Copyright (C) 2010-2011 Julien KAUFFMANN <julien.kauffmann@freelan.org>
+ *
+ * This file is part of libfreelan.
+ *
+ * libfreelan is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * libfreelan is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * In addition, as a special exception, the copyright holders give
+ * permission to link the code of portions of this program with the
+ * OpenSSL library under certain conditions as described in each
+ * individual source file, and distribute linked combinations
+ * including the two.
+ * You must obey the GNU General Public License in all respects
+ * for all of the code used other than OpenSSL.  If you modify
+ * file(s) with this exception, you may extend this exception to your
+ * version of the file(s), but you are not obligated to do so.  If you
+ * do not wish to do so, delete this exception statement from your
+ * version.  If you delete this exception statement from all source
+ * files in the program, then also delete it here.
+ *
+ * If you intend to use libfreelan in a commercial software, please
+ * contact me : we may arrange this for a small fee or no fee at all,
+ * depending on the nature of your project.
+ */
+
+/**
+ * \file core.hpp
+ * \author Julien KAUFFMANN <julien.kauffmann@freelan.org>
+ * \brief The freelan core class.
+ */
+
+#ifndef FREELAN_CORE2_HPP
+#define FREELAN_CORE2_HPP
+
+#include "configuration.hpp"
+#include "logger.hpp"
+
+#include <fscp/server.hpp>
+
+namespace freelan
+{
+	/**
+	 * \brief The core class.
+	 */
+	class core
+	{
+		public:
+
+			/**
+			 * \brief The constructor.
+			 * \param io_service The io_service to bind to.
+			 * \param configuration The configuration to use.
+			 * \param _logger The logger to use for logging.
+			 */
+			core(boost::asio::io_service& io_service, const freelan::configuration& configuration, const freelan::logger& _logger);
+
+		private:
+			boost::asio::io_service& m_io_service;
+	};
+}
+
+#endif /* FREELAN_CORE_HPP */
