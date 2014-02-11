4035b7fce47c0c314b06c71201756c42cc9b70e6
Julien Kauffmann <julien.kauffmann@freelan.org>
2014-02-11 20:52:20 +0100

    Added commented ways of running the daemon

--- run.sh
+++ run.sh
@@ -6,3 +6,5 @@ export LD_LIBRARY_PATH="${SCRIPTPATH}/install/lib"
 export DYLD_LIBRARY_PATH="${SCRIPTPATH}/install/lib"
 
 ${SCRIPTPATH}/install/bin/freelan $@
+#valgrind ${SCRIPTPATH}/install/bin/freelan $@
+#gdb --args ${SCRIPTPATH}/install/bin/freelan $@
