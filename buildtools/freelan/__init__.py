a7b03860874145d3c7092efb85c5e6ec4028e9a5
Julien Kauffmann <julien.kauffmann@freelan.org>
2014-05-06 18:58:41 +0200

    Removed useless buildtools directory

--- buildtools/freelan/__init__.py
+++
@@ -1,1 +0,0 @@
-"""The freelan module."""
--- buildtools/freelan/buildtools.py
+++
@@ -1,4 +0,0 @@
-"""A set of tools to ease FreeLAN related libraries."""
-
-from freelan.project import LibraryProject, ProgramProject, SampleProject
-from freelan.environment import Environment
