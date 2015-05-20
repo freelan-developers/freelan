467171067269e17b8ac9dd59615d5c3da101266c
Julien Kauffmann <julien.kauffmann@freelan.org>
2015-05-20 19:25:02 -0400

    Added more native tests

--- tests/test_native_calls.py
+++ tests/test_native_calls.py
@@ -19,32 +19,75 @@ class NativeCallsTests(TestCase):
 
     def test_malloc(self):
         result = native.freelan_malloc(6)
+        self.addCleanup(native.freelan_free, result)
 
         self.assertNotEqual(ffi.NULL, result)
-
-        native.freelan_free(result)
 
     def test_realloc(self):
         buf = native.freelan_malloc(6)
         result = native.freelan_realloc(buf, 12)
+        self.addCleanup(native.freelan_free, result)
 
         self.assertNotEqual(ffi.NULL, result)
-
-        native.freelan_free(result)
 
     def test_strdup(self):
         result = native.freelan_strdup("freelan")
+        self.addCleanup(native.freelan_free, result)
 
         self.assertEqual("freelan", ffi.string(result))
 
-        native.freelan_free(result)
-
-    def test_IPv4Address_from_string_simple(self):
-        result = native.freelan_IPv4Address_from_string(self.ectx, "1.2.4.8")
+    def test_acquire_error_context(self):
+        result = native.freelan_acquire_error_context()
+        self.addCleanup(native.freelan_release_error_context, result)
 
         self.assertNotEqual(ffi.NULL, result)
 
-        native.freelan_IPv4Address_free(result)
+    def test_error_context_reset(self):
+        ectx = native.freelan_acquire_error_context()
+        self.addCleanup(native.freelan_release_error_context, ectx)
+
+        native.freelan_error_context_reset(ectx)
+
+    def test_error_context_get_error_category(self):
+        ectx = native.freelan_acquire_error_context()
+        self.addCleanup(native.freelan_release_error_context, ectx)
+
+        result = native.freelan_error_context_get_error_category(ectx)
+        self.assertEqual(ffi.NULL, result)
+
+    def test_error_context_get_error_code(self):
+        ectx = native.freelan_acquire_error_context()
+        self.addCleanup(native.freelan_release_error_context, ectx)
+
+        result = native.freelan_error_context_get_error_code(ectx)
+        self.assertEqual(0, result)
+
+    def test_error_context_get_error_description(self):
+        ectx = native.freelan_acquire_error_context()
+        self.addCleanup(native.freelan_release_error_context, ectx)
+
+        result = native.freelan_error_context_get_error_description(ectx)
+        self.assertEqual(ffi.NULL, result)
+
+    def test_error_context_get_error_file(self):
+        ectx = native.freelan_acquire_error_context()
+        self.addCleanup(native.freelan_release_error_context, ectx)
+
+        result = native.freelan_error_context_get_error_file(ectx)
+        self.assertEqual(ffi.NULL, result)
+
+    def test_error_context_get_error_line(self):
+        ectx = native.freelan_acquire_error_context()
+        self.addCleanup(native.freelan_release_error_context, ectx)
+
+        result = native.freelan_error_context_get_error_line(ectx)
+        self.assertEqual(0, result)
+
+    def test_IPv4Address_from_string_simple(self):
+        result = native.freelan_IPv4Address_from_string(self.ectx, "1.2.4.8")
+        self.addCleanup(native.freelan_IPv4Address_free, result)
+
+        self.assertNotEqual(ffi.NULL, result)
 
     def test_IPv4Address_from_string_truncated(self):
         result = native.freelan_IPv4Address_from_string(self.ectx, "127.1")
@@ -63,10 +106,11 @@ class NativeCallsTests(TestCase):
 
     def test_IPv4Address_to_string_simple(self):
         str_value = "1.2.4.8"
+
         value = native.freelan_IPv4Address_from_string(self.ectx, str_value)
+        self.addCleanup(native.freelan_IPv4Address_free, value)
+
         result = native.freelan_IPv4Address_to_string(self.ectx, value)
-        native.freelan_IPv4Address_free(value)
+        self.addCleanup(native.freelan_free, result)
 
         self.assertEqual(str_value, ffi.string(result))
-
-        native.freelan_free(result)
