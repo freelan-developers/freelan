960aea09a1298f6ef7194e91e549aef4b037a989
Julien Kauffmann <julien.kauffmann@freelan.org>
2015-10-19 23:04:55 -0400

    Added compiler detection in the setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,55 @@ from setuptools import (
     setup,
 )
 
+import os
+import platform
+
+project_root = os.path.dirname(os.path.realpath(__file__))
+project_bin_dir = os.path.join(project_root, 'install', 'bin')
+project_include_dir = os.path.join(project_root, 'install', 'include')
+project_lib_dir = os.path.join(project_root, 'install', 'lib')
+
+
+def prepend_paths(name, *values):
+    """
+    Add a list of paths to a specified path-like environment variable.
+
+    :param name: The name of the environment variable to modify.
+    :param values: A list of paths to prepend to the environment variable.
+    """
+    paths = values + tuple(os.environ.get(name, '').split(os.pathsep))
+    os.environ[name] = os.pathsep.join(paths)
+
+
+if platform.system() == 'Windows':
+    # On Windows we detect the compiler and add it to the path if it isn't
+    # there already.
+    from shutil import which
+    from distutils.ccompiler import new_compiler
+    from distutils.sysconfig import customize_compiler
+
+    if not which('cl'):
+        compiler = new_compiler()
+        customize_compiler(compiler)
+        compiler.initialize()
+
+        prepend_paths('PATH', os.path.dirname(compiler.cc))
+        prepend_paths('INCLUDE', *compiler.include_dirs)
+        prepend_paths('LIB', *compiler.library_dirs)
+
+    prepend_paths('PATH', project_bin_dir)
+    prepend_paths('INCLUDE', project_include_dir)
+    prepend_paths('LIB', project_lib_dir)
+else:
+    prepend_paths('PATH', project_bin_dir)
+    prepend_paths('CPATH', project_include_dir)
+    prepend_paths('LIBRARY_PATH', project_lib_dir)
+
+    if platform.system() == 'Darwin':
+        prepend_paths('DYLD_LIBRARY_PATH', project_lib_dir)
+    else:
+        prepend_paths('LD_LIBRARY_PATH', project_lib_dir)
+
 setup(
     name='pyfreelan',
     version='0.0.1',
