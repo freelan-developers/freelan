ec27ed39de19c84266417de6b515a2fc1a91454f
Sebastien Vincent <sebastien.vincent@cppextrem.com>
2017-08-08 17:02:22 +0200

    Fixes path for start-client.sh/start-server.sh scripts.

--- scripts/start-client.sh
+++ scripts/start-client.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+MODE=debug
 USERNAME=${1:-client}
 
 shift
 
-./install/bin/freelan -f -d --client.enabled=yes --client.disable_peer_verification=yes --client.disable_host_verification=yes --fscp.listen_on=0.0.0.0:12001 --client.username=${USERNAME} --client.password="ok" --client.public_endpoint=0.0.0.0 --client.public_endpoint=:: --client.public_endpoint=localhost:12001 $@
+${SCRIPTPATH}/../build/${MODE}/bin/freelan -f -d --client.enabled=yes --client.disable_peer_verification=yes --client.disable_host_verification=yes --fscp.listen_on=0.0.0.0:12001 --client.username=${USERNAME} --client.password="ok" --client.public_endpoint=0.0.0.0 --client.public_endpoint=:: --client.public_endpoint=localhost:12001 $@
--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-./install/bin/freelan -f -d --server.enabled=yes --server.authentication_script=scripts/authenticate.sh $@
+SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+MODE=debug
+
+${SCRIPTPATH}/../build/${MODE}/bin/freelan -f -d --server.enabled=yes --server.authentication_script=${SCRIPTPATH}/scripts/authenticate.sh $@
